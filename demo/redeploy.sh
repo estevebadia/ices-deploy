08ca028cddbd9b379cac40530fb8df27551a863b
Esteve <badia.orive@gmail.com>
2020-04-06 10:51:42 +0200

    Demo deployment

---
+++ demo/redeploy.sh
@@ -0,0 +1,16 @@
+#!\bin\sh
+
+# Stop current container.
+docker-compose stop
+docker-compose rm -f
+
+# Build ices dev image
+cd ../../ices
+git pull
+docker build -t integralces:dev .
+
+# Build and run ices demo image
+cd ../ices-deploy/demo
+git pull
+docker-compose up -d --build
+
